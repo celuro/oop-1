7ba3fd168fe5ac3f041c8ab487499fa9445f7a62
mimikyumom <144652524+celuro@users.noreply.github.com>
2024-04-30 16:16:30 -0400

    added member operators + type cast + helper function

--- intro/Person.cpp
+++ intro/Person.cpp
@@ -14,7 +14,17 @@ Person();
 Person(const Person& other);
 Person& operator=(const Person& other);
 ~Person();
+//Pre-fix on age
+Person& operator++();
+//Post-fix on age
+Person& operator++(int);
+operator int() const;
+operator bool() const;
+const char* getName();
+int getAge();
 }
+
+std::ostream& display(std::ostream& os, const Person& p);
 
 #endif
 
@@ -66,4 +76,38 @@ Person::~Person(){
   if(name != nullptr){
   delete [] name;
   }
+
+//Pre-fix
+Person::operator++(){
+  age++;
+  return *this;
+}
+
+//Post-fix
+Person::operator++(int){
+Person temp(*this);
+age ++;
+return temp;
+}
+
+//Casting
+Person::operator double(){
+return age;
+}
+
+Person::operator bool(){
+return age > 18;
+}
+
+Person::getAge(){
+return age;
+}
+
+Person::getName(){
+return name;
+}
+
+std::ostream& display(std::ostream& os, const Person& p){
+os << "Name: " << p.getName() << "\n";
+os << "Age: " << p.getAge() << std::endl;
 }
