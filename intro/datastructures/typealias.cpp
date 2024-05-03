693df54e84f1b568071bd7f93c280d1c7fd1a4d7
mi <144652524+celuro@users.noreply.github.com>
2024-05-03 16:08:48 -0400

    Create typealias.cpp

---
+++ intro/datastructures/typealias.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+
+typedef int YEAR;
+YEAR current = 2024;
+
+int main() {
+    typedef int YEAR;
+    YEAR current = 2024;
+    std::cout << "The current year is: " << current << ".\n";
+    return 0;
+}
